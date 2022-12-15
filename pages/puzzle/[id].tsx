7fadef2d36aa05927e0ad3d9a3e473e2b9ba1597
Ajay Gandecha <ajgandecha@gmail.com>
2022-12-15 11:22:16 -0500

    Fixed error on invalid puzzle ID
    - Fixed an error for when an invalid puzzle ID is passed into [id].tsx.

--- pages/puzzle/[id].tsx
+++ pages/puzzle/[id].tsx
@@ -1,8 +1,6 @@
 import { PrismaClient, Puzzle } from "@prisma/client";
 import { GetStaticPaths, GetStaticProps } from "next";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { idText } from "typescript";
 import PuzzleBoard from "../../components/PuzzleBoard";
 
 interface PuzzleProps {
@@ -14,6 +12,7 @@ export default function PuzzleView({puzzles}: PuzzleProps) {
     const router = useRouter();
     const { id } = router.query;
 
+    
     return (
         <div className="h-screen bg-slate-100 dark:bg-slate-800">
 
@@ -28,7 +27,11 @@ export default function PuzzleView({puzzles}: PuzzleProps) {
             <main>
                 <div className="flex items-center justify-center mt-8">
                     {/* Puzzle Container */}
-                    <PuzzleBoard puzzle={puzzles.filter(puzzle => puzzle.id.toString() === id)[0]}/>
+                    {
+                        (puzzles.filter(puzzle => puzzle.id.toString() === id).length > 0) ?
+                        (<PuzzleBoard puzzle={puzzles.filter(puzzle => puzzle.id.toString() === id)[0]}/>) :
+                        (<p className="text-center font-mono">Invalid puzzle ID</p>)
+                    }
                 </div>
             </main>
 
